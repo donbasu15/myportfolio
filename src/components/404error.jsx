2efe397afd3607dd0447bb51c9c9924bb3986ea4
Don Basumatary <donbasty375@gmail.com>
2024-07-13 21:00:37 +0530

    background and animation

--- src/components/404error.jsx
+++ src/components/404error.jsx
@@ -2,7 +2,7 @@
 
 function Error(){
     return(
-        <div className="error-page fit">
+        <div className="error-page fit ani">
              <h1>ERR 404</h1>
              <h3>Page Not Found</h3>
         </div>
